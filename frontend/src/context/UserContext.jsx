8e60b9220cf45dadd06c847acd1036a93494bb7a
ritik chaurasia <ritikchaurasia0707@gmailcom>
2025-04-14 08:14:23 +0530

    context here

---
+++ frontend/src/context/UserContext.jsx
@@ -0,0 +1,39 @@
+import React, { createContext, useContext, useEffect, useState } from 'react'
+import { authDataContext } from './authContext.jsx';
+import axios from 'axios';
+
+export const userDataContext = createContext();
+const UserContext = ({children}) => {
+
+    let [userData, setUserData] = useState(null);
+
+    let {serverUrl} = useContext(authDataContext);
+
+    let [edit, setEdit] = useState(false);
+    
+    const getCurrentUser = async()=>{
+        try{
+            let result = await axios.get(serverUrl + "/api/user/currentuser", {withCredentials:true});
+            // console.log(result);
+            setUserData(result.data);
+        }
+        catch(error){
+            console.log(error);
+            setUserData(null);
+        } 
+    }
+    useEffect(()=>{
+        getCurrentUser();
+    },[])
+
+    const value = {userData, setUserData , edit, setEdit};
+  return (
+    <div>
+        <userDataContext.Provider value={value}>
+            {children}
+        </userDataContext.Provider>
+    </div>
+  )
+}
+
+export default UserContext;
