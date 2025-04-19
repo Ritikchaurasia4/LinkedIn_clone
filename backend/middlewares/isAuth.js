16db69275de3b5f3051d7e099ea82ea743bc5eaf
ritik chaurasia <ritikchaurasia0707@gmailcom>
2025-04-19 11:16:21 +0530

    authentication here

--- backend/middlewares/isAuth.js
+++ backend/middlewares/isAuth.js
@@ -1,19 +1,24 @@
-import jwt from 'jsonwebtoken';
+import jwt from "jsonwebtoken"
+import dotenv from "dotenv"
+dotenv.config()
+const isAuth=async (req,res,next)=>{
+    try {
+        let {token}=req.cookies
 
-const isAuth = async (req, res, next)=>{
-    try{
-        let {token} = req.cookies;
         if(!token){
-            return res.status(400).json({message:"token not found. user does't have token"})
+            return res.status(400).json({message:"user doesn't have token"})
         }
-        let verifyToken = await jwt.verify(token,process.env.JWT_SECRET);
+        let verifyToken= jwt.verify(token,process.env.JWT_SECRET)
         if(!verifyToken){
-            return res.status(400).json({message:"user does't have valid token"})
+            return res.status(400).json({message:"user doesn't have valid token"})
         }
-        req.userId = verifyToken.Id;
-        next();
-    }
-    catch(error){
-        return res.status(500).json({message:"Is auth errorm"})
+        
+        req.userId=verifyToken.userId
+        next()
+    } catch (error) {
+        console.log(error)
+        return res.status(500).json({message:"is auth error"})
     }
 }
+
+export default isAuth
