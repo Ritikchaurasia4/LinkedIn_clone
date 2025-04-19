08f6ed6dd4061fe466f664f8917e29c9406a6c76
ritik chaurasia <ritikchaurasia0707@gmailcom>
2025-04-19 11:14:44 +0530

    auth controller page here.

--- backend/controllers/auth.controllers.js
+++ backend/controllers/auth.controllers.js
@@ -1,113 +1,80 @@
-import genToken from "../config/token.js";
-import User from "../models/user.model.js";
-import bcrypt from "bcryptjs";
+import genToken from "../config/token.js"
+import User from "../models/user.model.js"
+import bcrypt from "bcryptjs"
+export const SignUp=async (req,res)=>{
+    try {
+        const {firstName,lastName,userName,email,password}=req.body
+       let existEmail=await User.findOne({email})
+       if(existEmail){
+        return res.status(400).json({message:"email already exist !"})
+       }
+       let existUsername=await User.findOne({userName})
+       if(existUsername){
+        return res.status(400).json({message:"userName already exist !"})
+       }
+       if(password.length<8){
+        return res.status(400).json({message:"password must be at least 8 characters"})
+       }
+      
+       let hassedPassword=await bcrypt.hash(password,10)
+      
+       const user=await User.create({
+        firstName,
+        lastName,
+        userName,
+        email,
+        password:hassedPassword
+       })
 
-export const SignUp = async (req, res) =>{
-    try{
-        const {firstName, lastName, userName, email, password} = req.body;
+       let token=await genToken(user._id)
+       res.cookie("token",token,{
+        httpOnly:true,
+        maxAge:7*24*60*60*1000,
+        sameSite:"strict",
+        secure:process.env.NODE_ENVIRONMENT==="production"
+       })
+      return res.status(201).json(user)
 
-        if(!firstName || !lastName || !userName || !email || !password){
-            return res.status(400).json({message: "All fields are required !"})
-        };
-        
-        let existEmail = await User.findOne({email});
-        if(existEmail){
-            return res.status(400).json({message: "Email already exists !"});
-        }
-        let existUser = await User.findOne({userName});
-        if(existUser){
-            return res.status(400).json({message: "Username already exists !"});
-        }
-
-        if(password.length < 8){
-            return res.status(400).json({message: "Password must be at least 8 characters long !"});
-        }
-
-        const hassedPassword = await bcrypt.hash(password, 10);
-
-        const user = await User.create({
-            firstName,
-            lastName,
-            userName,
-            email,
-            password: hassedPassword,
-        });
-
-        let token;
-        {
-            try{
-                token = await genToken(user._id);
-            }
-            catch(error){
-                console.log("User Token generation error", error.message);
-            }
-        }
-        res.cookie("token", token,{
-            httpOnly:true,
-            secure: process.env.NODE_ENV === 'production' ? true : false,
-            maxAge: 30 * 24 * 60 * 60 * 1000, // 30 day
-            sameSite: "strict"
-        })
-
-        return res.status(202).json(user);
-    }
-    catch(error){
-        console.log("User registration error", error);
-        return res.status(500).json({message: "Signup error"});
-    }
-}
-
-export const Login = async (req, res) =>{
-    try{
-        const {email, password} = req.body;
-
-        if(!email || !password){
-            return res.status(400).json({message: "All fields are required !"})
-        };
-
-        let user =  await User.findOne({email})
-        if(!user){
-            return res.status(400).json({message: "User does not exist !"});
-        }
-        
-        const isMatch = bcrypt.compare(password, user.password);
-        if(!isMatch){
-            return res.status(400).json({message: "Incorrect password!"});
-        }
-
-        let token;
-        {
-            try{
-                token = await genToken(user._id);
-            }
-            catch(error){
-                console.log("User Token generation error", error.message);
-            }
-        }
-        res.cookie("token", token,{
-            httpOnly:true,
-            secure: process.env.NODE_ENV === 'production' ? true : false,
-            maxAge: 30 * 24 * 60 * 60 * 1000, // 30 day
-            sameSite: "strict"
-        })
-
-        return res.status(202).json(user);
-
-        // return res.status(202).json({message:"User Signup successfully  "});
-    }
-    catch(error){
-        console.log("User Sign in  error", error);
-        return res.status(500).json({message: "Login error"});
-    }
-}
-
-export const Logout = async (req, res) =>{
-    try{
-        res.clearCookie("token");
-        return res.status(200).json({message:"User Logout successfully"});
-    }
-    catch(error){
+    } catch (error) {
         console.log(error);
-        return res.status(500).json({message:"logout error"});
+        return res.status(500).json({message:"signup error"})
+   
+    }
+}
+
+export const Login=async (req,res)=>{
+    try {
+        const {email,password}=req.body
+        let user=await User.findOne({email})
+        if(!user){
+         return res.status(400).json({message:"user does not exist !"})
+        }
+
+       const isMatch=await bcrypt.compare(password,user.password)
+       if(!isMatch){
+        return res.status(400).json({message:"incorrect password"})
+       }
+   
+        let token=await genToken(user._id)
+        res.cookie("token",token,{
+         httpOnly:true,
+         maxAge:7*24*60*60*1000,
+         sameSite:"strict",
+         secure:process.env.NODE_ENVIRONMENT==="production"
+        })
+       return res.status(200).json(user)
+    } catch (error) {
+        console.log(error);
+        return res.status(500).json({message:"login error"})
+    }
+}
+
+export const Logout=async (req,res)=>{
+    try {
+        res.clearCookie("token")
+        return res.status(200).json({message:"log out successfully"})
+    } catch (error) {
+        console.log(error);
+        return res.status(500).json({message:"logout error"})
     }
 }
