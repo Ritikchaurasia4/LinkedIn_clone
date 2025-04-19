064240bac93f2a0dcb888275e0629173e266a60c
ritik chaurasia <ritikchaurasia0707@gmailcom>
2025-04-19 12:11:38 +0530

    App here

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,16 +1,25 @@
-import React from 'react'
-import { Route, Routes } from 'react-router-dom'
+import React, { useContext } from 'react'
+import { Navigate, Route, Routes } from 'react-router-dom'
 import Home from './pages/Home'
 import Login from './pages/Login'
-import SignUp from './pages/SignUp'
+import { userDataContext } from './context/userContext'
+import Network from './pages/Network'
+import Profile from './pages/Profile'
+import Notification from './pages/Notification'
+import Signup from './pages/SignUp'
 
-const App = () => {
+function App() {
+  let {userData}=useContext(userDataContext)
   return (
-    <Routes>
-      <Route path='/' element={<Home/>}/>
-      <Route path='/signup' element={<SignUp/>}/>
-      <Route path='/login' element={<Login/>}/>
-    </Routes>
+   <Routes>
+    <Route path='/' element={userData?<Home/>:<Navigate to="/login"/>}/>
+    <Route path='/signup' element={userData?<Navigate to="/"/>:<Signup/>}/>
+    <Route path='/login' element={userData?<Navigate to="/"/>:<Login/>}/>
+    <Route path='/network' element={userData?<Network/>:<Navigate to="/login"/>}/>
+    <Route path='/profile' element={userData?<Profile/>:<Navigate to="/login"/>}/>
+    <Route path='/notification' element={userData?<Notification/>:<Navigate to="/login"/>}/>
+  
+   </Routes>
   )
 }
 
